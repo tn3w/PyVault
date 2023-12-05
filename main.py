718fd984838ef371a44e28e3c8b3528afe1c9bb1
tn3w <tn3wA8xxfuVMs2@proton.me>
2023-12-05 15:50:28 +0100

    Adding compressing

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from sys import exit
 if __name__ != "__main__":
     exit(1)
 
-from utils import clear_console, get_all_files_of_directory
+from utils import clear_console, get_all_files_of_directory, compress_structure, generate_random_string, get_password_strength,\
+                  is_password_safe
 import os
 from rich.console import Console
+from getpass import getpass
 
 CURRENT_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -53,5 +55,84 @@ while True:
                 structure, full_size = get_all_files_of_directory(path)
             
         CONSOLE.print("[green]~ Exploring the file structure... Done")
-        print("")
 
+        with CONSOLE.status("[green]Compression of all files..."):
+            structure = compress_structure(structure)
+
+        mission = None
+        options = ["Password encryption", "Public key encryption", "Password encryption and Public key encryption"]
+        selected_option = 0
+        while True:
+            clear_console()
+            print(f"Enter the file or folder path: {path}\n")
+            CONSOLE.print("[green]~ Exploring the file structure... Done")
+            CONSOLE.print("[green]~ Compression of all files... Done")
+            print("")
+
+            for i, option in enumerate(options):
+                if i == selected_option:
+                    print(f"[>] {option}")
+                else:
+                    print(f"[ ] {option}")
+            
+            key = input("\nSelect which encryption should be used (c to confirm): ")
+
+            if not key.lower() in ["c", "confirm"]:
+                if len(options) < selected_option + 2:
+                    selected_option = 0
+                else:
+                    selected_option += 1
+            else:
+                mission = selected_option
+                break
+        
+        encryption_method = options[mission]
+
+        password = None
+        publ_key_path = None
+
+        if mission in [0, 2]:
+            while True:
+                clear_console()
+                print(f"Enter the file or folder path: {path}\n")
+                CONSOLE.print("[green]~ Exploring the file structure... Done")
+                CONSOLE.print("[green]~ Compression of all files... Done")
+                print("\nUsing", encryption_method+"\n")
+                inputed_password = getpass("Please enter a strong password: ")
+
+                if inputed_password == "":
+                    with CONSOLE.status("[green]Generating a secure password..."):
+                        generated_password = generate_random_string(16)
+                        while not get_password_strength(generated_password) == 100:
+                            print(get_password_strength(generated_password))
+                            generated_password = generate_random_string(16)
+                    
+                    clear_console()
+                    CONSOLE.print("Your generated password is called:", f"[blue]{generated_password}")
+                    input("Press Enter, the password will no longer be displayed:")
+
+                    password = generated_password
+                    break
+                else:
+                    password_strength = get_password_strength(inputed_password)
+                    if password_strength < 50:
+                        CONSOLE.print("[red][Error]The given password is not strong enough")
+                        input("Enter: ") # FIXME: Decision on further procedure
+                        continue
+                    elif not is_password_safe(inputed_password):
+                        CONSOLE.print("[red][Error]The given password is not strong enough")
+                        input("Enter: ") # FIXME: Decision on further procedure
+                        continue
+                    else:
+                        strength_color = "red" if password_strength < 70 else "yellow" if password_strength < 90 else "green"
+                        CONSOLE.print(f"[{strength_color}]Password Strength: {str(password_strength)}/100%\n")
+
+                        repeated_password = getpass("Repeat Password: ")
+                        if not inputed_password == repeated_password:
+                            CONSOLE.print("[red][Error]The given password is not strong enough")
+                            input("Enter: ") # FIXME: Decision on further procedure
+                            continue
+                        else:
+                            password = inputed_password
+                            break
+        continue
--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 import os
+import gzip
+import secrets
+import re
+import hashlib
+from urllib import request, error
 
 LOGO = """
 ░█▀▀░█▀█░█▀▀░█▀▀░█▀█░█░█░█░█░█▀█░█░█░█░░░▀█▀
@@ -24,7 +29,98 @@ def get_all_files_of_directory(directory_path):
                     structure[full_path], add_to_full_size = get_all_files_of_directory(full_path)
                 full_size += add_to_full_size
             except:
-                continue
+                continue # FIXME: Error Handling
     except:
-        pass
+        pass # FIXME: Error Handling
     return structure, full_size
+
+def compress_file(file_path):
+    try:
+        with open(file_path, 'rb') as readable_file:
+            file_bytes = readable_file.read()
+    except:
+        return None # FIXME: Error Handling
+    try:
+        compressed_bytes = gzip.compress(file_bytes)
+
+        return compressed_bytes
+    except:
+        return file_bytes # FIXME: Error Handling
+
+def decompress_file(file_bytes):
+    try:
+        decompressed_bytes = gzip.decompress(file_bytes)
+
+        return decompressed_bytes
+    except:
+        return None # FIXME: Error Handling
+
+def compress_structure(structure: dict):
+    for path, object in structure.items():
+        if isinstance(object, dict):
+            object["content"] = compress_file(path)
+            structure[path] = object
+        else:
+            structure[path] = compress_structure(object)
+    return structure
+
+def generate_random_string(length: int, with_punctuation: bool = True, with_letters: bool = True) -> str:
+    """
+    Generates a random string
+
+    :param length: The length of the string
+    :param with_punctuation: Whether to include special characters
+    :param with_letters: Whether letters should be included
+    """
+
+    characters = "0123456789"
+
+    if with_punctuation:
+        characters += r"!\"#$%&'()*+,-.:;<=>?@[\]^_`{|}~"
+
+    if with_letters:
+        characters += "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    random_string = ''.join(secrets.choice(characters) for _ in range(length))
+    return random_string
+
+def get_password_strength(password: str) -> int:
+    """
+    Function to get a password strength from 0 (bad) to 100% (good)
+    :param password: The password to check
+    """
+    
+    strength = (len(password) * 62.5) / 16
+    
+    if strength > 70:
+        strength = 70
+        
+    if re.search(r'[A-Z]', password):
+        strength += 12.6
+    if re.search(r'[a-z]', password):
+        strength += 12.6
+    if re.search(r'[!@#$%^&*()_+{}\[\]:;<>,.?~\\]', password):
+        strength += 12.6
+    
+    if strength > 100:
+        strength = 100
+    return round(strength)
+
+def is_password_safe(password: str) -> bool:
+    password_sha1_hash = hashlib.sha1(password.encode()).hexdigest()
+    hash_prefix = password_sha1_hash[:5]
+
+    try:
+        url = f"https://api.pwnedpasswords.com/range/{hash_prefix}"
+        with request.urlopen(url) as response:
+            if response.status == 200:
+                response_content = response.read().decode('utf-8')
+
+                for sha1_hash in response_content.split("\n"):
+                    sha1_hash = sha1_hash.split(":")[0]
+                    if sha1_hash == password_sha1_hash:
+                        return False
+    except error.URLError as e:
+        pass  # FIXME: Error Handling
+
+    return True
